9fc070edc55e24b7f7636f2850308df559e7f7ab
elzwang <ewang3@stanford.edu>
2022-04-22 12:27:24 -0500

    empty spring project

---
+++ src/main/java/com/winnie/nasaapp/NasaAppApplication.java
@@ -0,0 +1,13 @@
+package com.winnie.nasaapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NasaAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NasaAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/winnie/nasaapp/NasaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.winnie.nasaapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NasaAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
